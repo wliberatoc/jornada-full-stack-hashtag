72786e2bfa0afd3393d27019f5234ba9e046509d
willian <willian.castro@nerdweb.com>
2025-02-18 21:10:09 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,8 @@ import "dotenv/config";
 import axios from "axios";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://jornada-full-stack-hashtag-sn3g.onrender.com/api";
 
 const responseArtisits = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
